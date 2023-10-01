ae3c41031bb5315228e07e98c9cdd5ae0424df42
Hamza <hamza.labzioui@gmail.com>
2023-10-01 15:17:18 +0200

    Fix add issue function

--- src/app/issue-list/issue-list.component.ts
+++ src/app/issue-list/issue-list.component.ts
@@ -9,13 +9,13 @@ import { Issue } from '../issue';
 })
 export class IssueListComponent implements OnInit {
 	issues: Issue[] = [];
-	showReportIsue = false;
+	showReportIssue = false;
 	constructor(private issueService: IssuesService) { }
 	ngOnInit(): void {
 		this.getIssues();
 	}
 	onCloseReport() {
-		this.showReportIsue = false;
+		this.showReportIssue = false;
 		this.getIssues();
 	}
 	private getIssues() {
--- src/app/issues.service.ts
+++ src/app/issues.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Issue } from './issue';
-import { issues as data } from 'src/assets/mock-issues';
+// import { issues as data } from 'src/assets/mock-issues';
 
 @Injectable({
 	providedIn: 'root'
@@ -9,7 +9,7 @@ export class IssuesService {
 	private issues: Issue[] = [];
 
 	constructor() {
-		this.issues = data;
+		// this.issues = data;
 	}
 	getPendingIssues(): Issue[] {
 		return this.issues.filter(issue => !issue.completed)
